affa99427be83757057f95812c07dd3759bbd858
Sarah Elzinga <sevans924@gmail.com>
2019-03-24 19:38:28 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -11,7 +11,8 @@ def third_challenge
         },
       :hero => {
         :name => "Romeo",
-        :age => "15"#your key/value pairs here
+        :age => "15",
+        :status => "alive"#your key/value pairs here
         },
       :hero_friends => [
         {
