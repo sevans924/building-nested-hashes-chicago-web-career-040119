22dfa541137a404f8ee3e2eec3b202fb9fd0c699
Sarah Elzinga <sevans924@gmail.com>
2019-03-24 19:24:16 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,16 +1,16 @@
 def second_challenge
   epic_tragedy = {
     :montague => {
-      :patriarch => {}
-      :matriarch => {}
-      :hero => {}
-      :hero_friends => []#your key/value pairs here!
+      :patriarch {}
+      :matriarch {}
+      :hero {}
+      :hero_friends []#your key/value pairs here!
       },
     :capulet => {
-      :patriarch => {}
-      :matriarch => {}
-      :hero => {}
-      :hero_friends => []
+      :patriarch {}
+      :matriarch {}
+      :hero {}
+      :hero_friends []
       #your key/value pairs here!
       }}
 
